8821f5af29f9a2417b73475a5fc2bc03715a5ef0
Chris Pearce <chris@gaia.family>
2021-09-25 10:29:38 +0100

    add rewrites

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  rewrites() {
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: 'https://legacy.gaiafamily.com/:path*',
+        },
+      ],
+    }
+  },
 }
